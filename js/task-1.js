6a80a84995537fd9fa5e27f89b60ee2bdd9217b5
Yuliia Babkina <yulichka.babkina1312@gmail.com>
2024-09-07 16:52:14 +0300

    goit-js-hw-04

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,20 @@
 "use strict";
 
-function slugify(title) {
-    return title.toLowerCase().split(" ").join("-");
+function isEnoughCapacity(products, containerSize) {
+    let totalCount = 0;
+    const productsArray = Object.values(products);
+
+    for (const product of productsArray) {
+        totalCount += product;
+    }
+
+    return totalCount <= containerSize;
 }
 
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
